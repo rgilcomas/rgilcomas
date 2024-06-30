235c9652494e792f72c5adabe8f69929fa057385
rgilcomas <rgilcomas@gmail.com>
2024-06-30 14:10:21 +0200

    Updates

---
+++ _app/immutable/chunks/paths.ZgeQd2OG.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1svv5y1?.base??"/rgc-web-gh-pages",e=globalThis.__sveltekit_1svv5y1?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/entry/start.xCLWDo3A.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.dvCEND-e.js";export{t as start};
